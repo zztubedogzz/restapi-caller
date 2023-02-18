a4cf26521005c7d706c5417614a70535d4f2ace8
zztubedogzz <zztubedogzz@gmail.com>
2023-02-18 21:46:31 +0100

    Simplifying code. Implement post,put,patch methods for curl calls. No usable function for it jet.

--- restapicall.php
+++ restapicall.php
@@ -3,6 +3,7 @@ class api{
 	//create base url reference, if not set use localhost
 	private $BASE_URL;
 	private $debug;
+	//first strip the "/" from the end
 	function __construct($url = "localhost", $verbose = false){
 		if (substr($url, -1)=="/") {
 			$url = substr($url, 0, -1);
@@ -11,6 +12,28 @@ class api{
 			$this->debug = true;
 		}
 		$this->BASE_URL = $url;
+	}
+
+	//this function deals with the basic curl http calls
+	function curlBase($entrypoint,$method = "GET",$postfields = null {
+		$call = $this->fixEntrypoint($entrypoint);
+		$curl = curl_init();
+		if (isset($header)) {
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array($header));
+		}
+		if ($method == "POST" || $method == "PUT" || $method == "PATCH") {
+			if ($postfields==null) {
+				die("No post data!");
+			}
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
+		}
+		curl_setopt($curl, CURLOPT_URL, $call);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+		$output = curl_exec($curl);
+		curl_close($curl);
+		return $output;
 	}
 
 	//if user entered entrypoint without leading "/" insert it.
@@ -28,26 +51,14 @@ class api{
 		return $return;
 	}
 
-	//get raw data from BASE_URL/$entrypoint
-	function getRaw($entrypoint) {
-		$call = $this->fixEntrypoint($entrypoint);
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $call);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		$output = curl_exec($curl);
-		curl_close($curl);
-		return $output;
-	}
-
-	//get json data from BASE_URL/$entrypoint
-	function get($entrypoint) {
-		$call = $this->fixEntrypoint($entrypoint);
-		$curl = curl_init();
-		curl_setopt($curl, CURLOPT_URL, $call);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-		$output = curl_exec($curl);
-		curl_close($curl);
-		return json_decode($output);
+	//get raw data from BASE_URL/$entrypoint in prettyfied json, if $raw is true then it returns raw response.
+	function get($entrypoint, $method = "GET", $postfields = array(), $raw = true) {
+		curlBase($entrypoint);
+		if ($raw) {
+			return json_decode($output);
+		} else {
+			return $output
+		}
 	}
 }
 
